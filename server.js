3a40484ea76e739df19061ee9bb6bf96851e93a3
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2024-11-06 22:08:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
